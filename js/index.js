d768779821bc23a997f9087aa0eb986a39a76aad
pauljevicj <pauljevicj@gmail.com>
2024-10-21 00:56:54 +0200

    Dodata implementacija za burger meni

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const logo = document.querySelector(".logo");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  logo.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  logo.classList.remove("active");
+}));
